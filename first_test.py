acfed2b5ea7e06fb444ce78e0fbbe9db261f83b4
suresh-talloju <tallojusuresh109@gmail.com>
2026-05-05 17:38:10 +0530

    Add Playwright test to fetch Google page title

--- first_test.py
+++ first_test.py
@@ -1,1 +1,12 @@
+from playwright.sync_api import sync_playwright
 
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    
+    page.goto("https://www.google.com")
+    
+    title = page.title()
+    print("Page Title:", title)
+    
+    browser.close()
